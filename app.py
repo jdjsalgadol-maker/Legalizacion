39734357b6509d8a8f4904e941f63185f9a8f2ac
jdjsalgadol-maker <jdjsalgadol@gmail.com>
2026-06-12 08:21:44 -0500

    Update page title in app configuration

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime, date
 from difflib import get_close_matches
 
 # Configuración de página inicial
-st.set_page_config(page_title="SICOMP IA - Motor Cambiario", page_icon="🛃", layout="wide")
+st.set_page_config(page_title="Pre validador Siscomp - ", page_icon="🛃", layout="wide")
 
 # Estilos personalizados en Dark Mode
 st.markdown("""
